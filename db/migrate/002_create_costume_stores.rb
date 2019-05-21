165c9d8e96e2eaed9970c9d74db0a1cd907e07fd
Herna Michel <labellehernamichel@yahoo.fr>
2019-05-21 16:20:07 +0000

    Done.

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,15 @@
 # Create your costume_stores migration here
+
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  def change
+    create_table :costume_stores do |value|
+      value.string :name
+      value.string :location
+      value.integer :costume_inventory
+      value.integer :num_of_employees
+      value.boolean :still_in_business
+      value.datetime :opening_time
+      value.datetime :closing_time
+    end
+  end
+end 
